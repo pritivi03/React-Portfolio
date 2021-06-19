4a961836fdb2e9a65074602b1e45ae405669ddad
pritivi03 <37942762+pritivi03@users.noreply.github.com>
2021-06-19 01:28:53 -0500

    Pending Changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 
+
+
+
 class App extends Component {
   render() {
     return (
