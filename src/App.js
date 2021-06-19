9f66e847acb210f66f19a0d0186c3cb62552ffa3
pritivi03 <37942762+pritivi03@users.noreply.github.com>
2021-06-19 01:34:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 
+
+
 class App extends Component {
   render() {
     return (
