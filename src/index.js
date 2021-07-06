4840bbfc5a8458db095316c488d1acf771c5a4c3
pritivi03 <37942762+pritivi03@users.noreply.github.com>
2021-07-06 01:32:18 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+
+
+
 //Added a test
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
